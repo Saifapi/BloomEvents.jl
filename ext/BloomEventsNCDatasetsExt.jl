35325b43870bf421a8b5d52fa25ab9630728242b
Saifapi <apithesaif@gmail.com>
2026-09-08 00:38:36 +0600

    Fix NCDatasets extension; add category stack NetCDF export

--- ext/BloomEventsNCDatasetsExt.jl
+++ ext/BloomEventsNCDatasetsExt.jl
@@ -4,6 +4,25 @@ using BloomEvents
 using NCDatasets
 using Dates
 
+# -------------------------
+# Helper: write a 3D metric (year, lat, lon)
+# -------------------------
+function _write_metric!(ds::NCDataset,
+                        name::String,
+                        data::AbstractArray{<:Real,3},
+                        units::String,
+                        long_name::String)
+    v = defVar(ds, name, Float32, ("year","latitude","longitude"))
+    v.attrib["_FillValue"] = Float32(NaN)
+    v.attrib["units"] = units
+    v.attrib["long_name"] = long_name
+    v[:, :, :] = Float32.(data)
+    return nothing
+end
+
+# -------------------------
+# Loader: single cube file with optional time subsetting
+# -------------------------
 function BloomEvents.load_chl_cube_netcdf(filepaths::AbstractVector{<:AbstractString};
                                          varname::AbstractString = "CHL",
                                          timename::AbstractString = "time",
@@ -18,7 +37,6 @@ function BloomEvents.load_chl_cube_netcdf(filepaths::AbstractVector{<:AbstractSt
 
     ds = NCDataset(paths[1])
 
-    # Read full time vector (small) then subset indices
     timev = ds[timename][:]
     dates_full = Date.(timev)
 
@@ -38,7 +56,7 @@ function BloomEvents.load_chl_cube_netcdf(filepaths::AbstractVector{<:AbstractSt
 
     var = ds[varname]
 
-    # Infer dimension order from FULL variable shape (before subsetting)
+    # infer dimension order from variable size (your file is lon × lat × time)
     szfull = size(var)
     ntfull = length(dates_full)
     nlat = length(lat)
@@ -50,15 +68,16 @@ function BloomEvents.load_chl_cube_netcdf(filepaths::AbstractVector{<:AbstractSt
     (it === nothing || iy === nothing || ix === nothing) &&
         throw(ArgumentError("Cannot infer dimension order for $varname with size $szfull"))
 
-    # Read only selected time indices (still in file order)
-    # This yields a 3D array with same dim order as var, but time length = length(tidx)
+    nd = ndims(var)
+    nd == 3 || throw(ArgumentError("Expected a 3D variable for $varname, got ndims=$nd"))
+
     inds = ntuple(d -> d == it ? tidx : Colon(), 3)
-    A = var[inds...]   # keeps 3D shape (e.g., lon × lat × time_subset)
+    A = var[inds...]                      # still in file order (e.g., lon × lat × time_subset)
 
     close(ds)
 
-    # Permute to (time, lat, lon) for core pipeline
-    Ap = permutedims(A, (it, iy, ix))  # -> (time_subset, lat, lon)
+    # permute to time × lat × lon
+    Ap = permutedims(A, (it, iy, ix))
 
     nt = length(dates)
     chl3d = Array{Union{Missing,Float64}}(undef, nt, nlat, nlon)
@@ -80,20 +99,9 @@ function BloomEvents.load_chl_cube_netcdf(filepaths::AbstractVector{<:AbstractSt
     return dates, chl3d, lat, lon
 end
 
-# ---- NetCDF stack writer (unchanged) ----
-function _write_metric!(ds::NCDataset,
-                        name::String,
-                        data::AbstractArray{<:Real,3},
-                        units::String,
-                        long_name::String)
-    v = defVar(ds, name, Float32, ("year","latitude","longitude"))
-    v.attrib["_FillValue"] = Float32(NaN)
-    v.attrib["units"] = units
-    v.attrib["long_name"] = long_name
-    v[:, :, :] = Float32.(data)
-    return nothing
-end
-
+# -------------------------
+# Writer: metric stacks -> NetCDF
+# -------------------------
 function BloomEvents.write_metric_stack_netcdf(path::AbstractString,
                                               lat::AbstractVector,
                                               lon::AbstractVector,
@@ -137,7 +145,56 @@ function BloomEvents.write_metric_stack_netcdf(path::AbstractString,
     ds.attrib["Conventions"] = "CF-1.11"
     ds.attrib["title"] = "Bloom metric stacks (BloomEvents.jl)"
     ds.attrib["history"] = "Created by BloomEvents.jl write_metric_stack_netcdf"
+    for (k,v) in global_attrib
+        ds.attrib[k] = v
+    end
 
+    close(ds)
+    return path
+end
+
+# -------------------------
+# Writer: category day-count stacks -> NetCDF
+# -------------------------
+function BloomEvents.write_category_stack_netcdf(path::AbstractString,
+                                                lat::AbstractVector,
+                                                lon::AbstractVector,
+                                                cats;
+                                                global_attrib::Dict{String,Any}=Dict{String,Any}())
+
+    years = cats.years
+    ny = length(years)
+    nlat = length(lat)
+    nlon = length(lon)
+
+    ds = NCDataset(path, "c")
+
+    defDim(ds, "year", ny)
+    defDim(ds, "latitude", nlat)
+    defDim(ds, "longitude", nlon)
+
+    vyear = defVar(ds, "year", Int32, ("year",))
+    vyear.attrib["long_name"] = "Year"
+    vyear[:] = Int32.(years)
+
+    vlat = defVar(ds, "latitude", Float32, ("latitude",))
+    vlat.attrib["standard_name"] = "latitude"
+    vlat.attrib["units"] = "degrees_north"
+    vlat[:] = Float32.(lat)
+
+    vlon = defVar(ds, "longitude", Float32, ("longitude",))
+    vlon.attrib["standard_name"] = "longitude"
+    vlon.attrib["units"] = "degrees_east"
+    vlon[:] = Float32.(lon)
+
+    _write_metric!(ds, "days_likely",  cats.days_likely,  "days", "Days in Likely-to-Bloom category")
+    _write_metric!(ds, "days_bloom",   cats.days_bloom,   "days", "Days in Bloom category")
+    _write_metric!(ds, "days_intense", cats.days_intense, "days", "Days in Intense Bloom category")
+    _write_metric!(ds, "days_extreme", cats.days_extreme, "days", "Days in Extreme Bloom category")
+
+    ds.attrib["Conventions"] = "CF-1.11"
+    ds.attrib["title"] = "Bloom category day-count stacks (BloomEvents.jl)"
+    ds.attrib["history"] = "Created by BloomEvents.jl write_category_stack_netcdf"
     for (k,v) in global_attrib
         ds.attrib[k] = v
     end
