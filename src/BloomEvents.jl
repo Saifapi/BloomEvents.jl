d2a58b7cec29fe260dc72d5c934e3dbdc85e6021
Saifapi <apithesaif@gmail.com>
2026-09-09 01:52:34 +0600

    ok

--- src/BloomEvents.jl
+++ src/BloomEvents.jl
@@ -58,5 +58,6 @@ export write_category_stack_netcdf
 export plot_category_panels_year, plot_category_panels_mean, plot_category_panels_trend
 export category_event_day_stack
 export event_peak_category_stack
+export write_peak_category_stack_netcdf
 
 end # module BloomEvents
